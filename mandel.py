4ccc6f06953631b1a9e5444b346d68ac834ba160
Blade Nelson <powderblock@gmail.com>
2014-10-12 13:18:47 -0400

    Improve comments, remove import math
    Improve comments
    Remove import math. Aren't using anything in math, so just remove it.

--- mandel.py
+++ mandel.py
@@ -1,10 +1,10 @@
-import math
 import sys
 from collections import *
 
+# Width an height of output image
 width, height = 256, 256
 
-# Named tuple for holding image information
+# Named tuple for holding pixel information
 pixel = namedtuple('pixel', ['r', 'g', 'b'])
 imageArray = []
 
@@ -12,14 +12,22 @@ magnify = 1.0
 max_iterations = 16
 # Header infomation:
 print("P6")
+# width and height are image size
+# 255 (third number) is color depth of output image.
 print str(width)+" "+str(height)+"\n255\n"
+# Go through each pixel in the image:
 for hy in range(0, height):
 	for hx in range(0, width):
 		c = z = (hx/float(width)-.5)*3-0.7 + (hy/float(height)-.5)*3j
+		# max_iterations + 1 to make sure we _can_ reach max_iterations:
 		for i in range(0, (max_iterations + 1)):
 			z = z*z+c
+			# if it is diviriging, break
 			if abs(z)>2:break
-		if(i < max_iterations): imageArray.append(pixel(0, 0, i * 4))
+		# Make the edge of the set colored. (R, G, B)
+		if(i < max_iterations): imageArray.append(pixel(0, 0, i * 16))
+		#It is is in the set, color it black
 		else: imageArray.append(pixel(0, 0, 0))
+# Once we have all of the pixels in the image processed, output their values:
 for i in range(0, len(imageArray)):
 	sys.stdout.write("%c%c%c" % (imageArray[i].r,imageArray[i].g,imageArray[i].b))
