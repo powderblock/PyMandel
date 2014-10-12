95be44eee4a8369a6fee1a224add0b05e0ae44d5
Blade Nelson <powderblock@gmail.com>
2014-10-12 13:07:07 -0400

    Implement complex numbers
    In mandel.py:
    -Implement complex numbers.
    -Compress width and height into one line.
    -Remove imageSize var, no need since we are just appending the tuple.
    
    Add small.py
    Add small_tweet.py

--- mandel.py
+++ mandel.py
@@ -2,30 +2,24 @@ import math
 import sys
 from collections import *
 
-width = 64; height = 64
-imageSize = width * height
+width, height = 256, 256
 
 # Named tuple for holding image information
 pixel = namedtuple('pixel', ['r', 'g', 'b'])
 imageArray = []
 
 magnify = 1.0
-max_iterations = 64
+max_iterations = 16
 # Header infomation:
 print("P6")
 print str(width)+" "+str(height)+"\n255\n"
 for hy in range(0, height):
-    for hx in range(0, width):
-        cx = ((float(hx) / float(width)) - 0.5) / magnify*3.0-0.7
-        cy = ((float(hy) / float(height)) - 0.5) / magnify*3.0
-        zx = cx
-        zy = cy
-        for i in range(0, max_iterations):
-            tmp = zx * zx - zy * zy + cx;
-            zy = (2.0 * zx)*zy+cy;
-            zx = tmp;
-            if(zx * zx + zy * zy > 4.0): break
-        if(i < max_iterations): imageArray.append(pixel(0, 0, i * 4))
-        else: imageArray.append(pixel(0, 0, 0))
+	for hx in range(0, width):
+		c = z = (hx/float(width)-.5)*3-0.7 + (hy/float(height)-.5)*3j
+		for i in range(0, (max_iterations + 1)):
+			z = z*z+c
+			if abs(z)>2:break
+		if(i < max_iterations): imageArray.append(pixel(0, 0, i * 4))
+		else: imageArray.append(pixel(0, 0, 0))
 for i in range(0, len(imageArray)):
-    sys.stdout.write("%c%c%c" % (imageArray[i].r,imageArray[i].g,imageArray[i].b))
+	sys.stdout.write("%c%c%c" % (imageArray[i].r,imageArray[i].g,imageArray[i].b))
---
+++ small_tweet.py
@@ -0,0 +1,8 @@
+w,h,m=256,256,64;print("P3"+str(w)+" "+str(h)+" 255")
+for k in range(0,h):
+ for b in range(0,w):
+  c=z=(b/float(w)-.5)*3-.7+(k/float(h)-.5)*3j
+  for i in range(0,(m+1)):
+    z=z*z+c
+    if abs(z)>2:break
+  print 0,0,i*4 if i<m else 0
